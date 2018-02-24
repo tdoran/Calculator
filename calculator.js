9a0a403bac528a983541344db2e40e35229e7289
Tom D <tomgdoran@gmail.com>
2018-02-24 20:26:24 +0000

    Fixed some bugs in number display
    Fixed the presence of a 0 at the start of calculations on the display

--- calculator.js
+++ calculator.js
@@ -1,51 +1,73 @@
+//Query Selectors//
 var numBtns = document.querySelectorAll(".numButton");
-var numDisplay = document.querySelector("#numDisplay")
-var ansDisplay = document.querySelector("#ansDisplay")
-var equalsBtn = document.querySelector("#equals")
-var resetBtn = document.querySelector("#reset")
-var deleteBtn = document.querySelector("#delete")
+var numDisplay = document.querySelector("#numDisplay");
+var ansDisplay = document.querySelector("#ansDisplay");
+var equalsBtn = document.querySelector("#equals");
+var resetBtn = document.querySelector("#reset");
+var deleteBtn = document.querySelector("#delete");
+var operatorBtns = document.querySelectorAll(".operatorBtn");
 
+//Variables//
 var mySum = "";
-
 var equalsPressed = false;
 
+//Add click listeners to numbers//
 for(var i=0;i<numBtns.length;i++){
-    if(numBtns[i].textContent==="x"){
         numBtns[i].addEventListener("click", function(){
+            ansDisplay.style.display="none";
+            numDisplay.style.display="block"
+            ansDisplay.textContent = "";
+
+            if(numDisplay.textContent==="0"){
+                numDisplay.textContent =this.textContent;
+            }
+            else{
+            numDisplay.textContent = numDisplay.textContent + this.textContent;
+            }
+            mySum = mySum + this.textContent;
+            
+            console.log(mySum);
+        })  
+}
+
+//Add click listeners to operators (excluding equals)//
+for(var i=0;i<operatorBtns.length;i++){
+    if(operatorBtns[i].textContent==="x"){
+        operatorBtns[i].addEventListener("click", function(){
             ansDisplay.textContent="";
             numDisplay.textContent = numDisplay.textContent + "x";
             mySum = mySum + "*";
             console.log(mySum);
         });
     }
-    else if(numBtns[i].textContent==="÷"){
-        numBtns[i].addEventListener("click", function(){
+    else if(operatorBtns[i].textContent==="÷"){
+        operatorBtns[i].addEventListener("click", function(){
             ansDisplay.textContent = "";
             numDisplay.textContent = numDisplay.textContent + "÷";
             mySum = mySum + "/";
 
             console.log(mySum);
             })
-    }    
-    else{
-        numBtns[i].addEventListener("click", function(){
-            ansDisplay.style.display="none";
-            numDisplay.style.display="block"
-            ansDisplay.textContent = "";
-            numDisplay.textContent = numDisplay.textContent + this.textContent;
-            mySum = mySum + this.textContent;
+    }  
+    else {operatorBtns[i].addEventListener("click", function(){
+        ansDisplay.style.display="none";
+        numDisplay.style.display="block"
+        ansDisplay.textContent = "";
+        numDisplay.textContent = numDisplay.textContent + this.textContent;
+        mySum = mySum + this.textContent;
 
-            console.log(mySum);
-        })
-    }   
+        console.log(mySum);
+        }) 
+    }      
 }
 
+//Add click listener to equals//
 equalsBtn.addEventListener("click", function(){
     numDisplay.textContent = "";
     numDisplay.style.display="none";
     ansDisplay.style.display="block"
-    ansDisplay.textContent = eval(mySum);
-    mySum = eval(mySum);
+    ansDisplay.textContent = eval(mySum).toString();
+    mySum = eval(mySum).toString();
     equalsPressed = true;
     console.log('this is mySum');
     console.log(mySum);
@@ -53,13 +75,17 @@ equalsBtn.addEventListener("click", function(){
     console.log(ansDisplay.textContent);
 });
 
+//Add click listener to reset ('C')//
 resetBtn.addEventListener("click", function(){
+    numDisplay.style.display="block";
+    ansDisplay.style.display="none";
     numDisplay.textContent="0";
     ansDisplay.textContent="";
-    mySum="0+";
+    mySum="";
     console.log(mySum);
 });
 
+//Add click listener to delete//
 deleteBtn.addEventListener("click", function(){
     var splitDisplay = numDisplay.textContent.split("");
     console.log(splitDisplay);
