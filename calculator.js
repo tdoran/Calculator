bf7073696e749eaddaa00a0c76498ac258cee2df
Tom D <tomgdoran@gmail.com>
2018-02-24 22:14:33 +0000

    Fixed various bugs
    -No longer allow multiple consecutive operator inputs (e.g. '+++++")
    
    -Decimal points can now only be used once per number (i.e. until a button that is not 0-9 is pressed)
    
    -A decimal input without a preceding number or after pressing equals will display as "0."

--- calculator.js
+++ calculator.js
@@ -6,69 +6,153 @@ var equalsBtn = document.querySelector("#equals");
 var resetBtn = document.querySelector("#reset");
 var deleteBtn = document.querySelector("#delete");
 var operatorBtns = document.querySelectorAll(".operatorBtn");
+var decimalBtn = document.querySelector("#decimal")
 
 //Variables//
 var mySum = "";
 var equalsPressed = false;
+var operatorPressed = false;
+var decimalPressed = false;
+
+//convert "*" and "/" to "x" and "÷"//
+function replaceMultDivide(sumString){
+    splitSum = sumString.split("");
+    for(var i=0;i<splitSum.length;i++){
+        if(splitSum[i]==="*"){
+            splitSum.splice(i, 1, 'x');
+        }
+        if(splitSum[i]==="/"){
+            splitSum.splice(i, 1, '÷');
+        }
+        if(splitSum[i]==="."&&splitSum[i-1]!==/([1-9])/){
+            splitSum.splice(i, 1, '0.')
+        }
+    }
+        return splitSum.join("");
+    }
 
 //Add click listeners to numbers//
 for(var i=0;i<numBtns.length;i++){
         numBtns[i].addEventListener("click", function(){
-            ansDisplay.style.display="none";
-            numDisplay.style.display="block"
+/*             ansDisplay.style.display="none";
+            numDisplay.style.display="block" */
             ansDisplay.textContent = "";
-
-            if(numDisplay.textContent==="0"){
-                numDisplay.textContent =this.textContent;
+            if(equalsPressed===false){
+                if(numDisplay.textContent==="0"){
+                    numDisplay.textContent =this.textContent;
+                }
+                else{
+                numDisplay.textContent = numDisplay.textContent + this.textContent;
+                }
+                mySum = mySum + this.textContent;
             }
-            else{
-            numDisplay.textContent = numDisplay.textContent + this.textContent;
+            if(equalsPressed===true){
+                if(numDisplay.textContent==="0"){
+                    numDisplay.textContent =this.textContent;
+                    equalsPressed=false;
+                }
+                else{
+                numDisplay.textContent = this.textContent;
+                }
+                mySum = this.textContent;
+                equalsPressed=false;
             }
-            mySum = mySum + this.textContent;
-            
+            operatorPressed=false;
             console.log(mySum);
         })  
 }
 
+//Add click listner to decimal//
+decimalBtn.addEventListener("click", function(){
+    ansDisplay.textContent = "";
+    var splitDisplay = numDisplay.textContent.split("");
+    
+    if(equalsPressed===false){
+        if(numDisplay.textContent==="0"){
+            numDisplay.textContent = "0" + this.textContent;
+            mySum = mySum + this.textContent;
+        }
+        else if(decimalPressed===false){
+            if(splitDisplay[splitDisplay.length-1]!==/([1-9])/){
+                numDisplay.textContent = numDisplay.textContent + "0" + this.textContent;
+                mySum = mySum + this.textContent;
+            }
+            else{numDisplay.textContent = numDisplay.textContent + this.textContent;
+            mySum = mySum + this.textContent;
+            }
+
+        }
+    }
+    
+    if(equalsPressed===true||numDisplay.textContent==="0"){
+            numDisplay.textContent = "0" + this.textContent;
+            equalsPressed=false;
+            decimalPressed=true;
+            operatorPressed=false;
+            console.log(mySum);
+    }
+});
+
 //Add click listeners to operators (excluding equals)//
 for(var i=0;i<operatorBtns.length;i++){
+
+    //multiply button//
     if(operatorBtns[i].textContent==="x"){
-        operatorBtns[i].addEventListener("click", function(){
-            ansDisplay.textContent="";
-            numDisplay.textContent = numDisplay.textContent + "x";
-            mySum = mySum + "*";
-            console.log(mySum);
-        });
+            operatorBtns[i].addEventListener("click", function(){
+                if(operatorPressed===false){
+                    ansDisplay.textContent="";
+                    numDisplay.textContent = numDisplay.textContent + "x";
+                    mySum = mySum + "*";
+                    equalsPressed=false;
+                    operatorPressed=true;
+                    decimalPressed=false;
+                    console.log(mySum);
+                    }
+                });
     }
+
+    //divide button//
     else if(operatorBtns[i].textContent==="÷"){
         operatorBtns[i].addEventListener("click", function(){
-            ansDisplay.textContent = "";
-            numDisplay.textContent = numDisplay.textContent + "÷";
-            mySum = mySum + "/";
-
-            console.log(mySum);
-            })
+                if(operatorPressed===false){
+                ansDisplay.textContent = "";
+                numDisplay.textContent = numDisplay.textContent + "÷";
+                mySum = mySum + "/";
+                equalsPressed=false;
+                operatorPressed=true;
+                decimalPressed=false;
+                console.log(mySum);
+                }
+            });
     }  
-    else {operatorBtns[i].addEventListener("click", function(){
-        ansDisplay.style.display="none";
-        numDisplay.style.display="block"
-        ansDisplay.textContent = "";
-        numDisplay.textContent = numDisplay.textContent + this.textContent;
-        mySum = mySum + this.textContent;
 
-        console.log(mySum);
-        }) 
+    //addittion and subtraction buttons//
+    else {operatorBtns[i].addEventListener("click", function(){
+        if(operatorPressed===false){
+    /*         ansDisplay.style.display="none";
+            numDisplay.style.display="block" */
+            ansDisplay.textContent = "";
+            numDisplay.textContent = numDisplay.textContent + this.textContent;
+            mySum = mySum + this.textContent;
+            equalsPressed=false;
+            operatorPressed=true;
+            decimalPressed=false;
+            console.log(mySum);
+        }
+        });
     }      
 }
 
 //Add click listener to equals//
 equalsBtn.addEventListener("click", function(){
-    numDisplay.textContent = "";
-    numDisplay.style.display="none";
+    numDisplay.textContent = eval(mySum).toString();;
+    numDisplay.style.display="block";
     ansDisplay.style.display="block"
-    ansDisplay.textContent = eval(mySum).toString();
+    ansDisplay.textContent = replaceMultDivide(mySum) + "=";
     mySum = eval(mySum).toString();
     equalsPressed = true;
+    decimalPressed=false;
+    operatorPressed=false;
     console.log('this is mySum');
     console.log(mySum);
     console.log('this is ansDisplay');
@@ -77,30 +161,41 @@ equalsBtn.addEventListener("click", function(){
 
 //Add click listener to reset ('C')//
 resetBtn.addEventListener("click", function(){
-    numDisplay.style.display="block";
-    ansDisplay.style.display="none";
+/*     numDisplay.style.display="block";
+    ansDisplay.style.display="none"; */
     numDisplay.textContent="0";
     ansDisplay.textContent="";
     mySum="";
+    decimalPressed=false;
+    operatorPressed=false;
     console.log(mySum);
 });
 
 //Add click listener to delete//
 deleteBtn.addEventListener("click", function(){
-    var splitDisplay = numDisplay.textContent.split("");
-    console.log(splitDisplay);
-    splitDisplay.pop();
-    var deletedDisplay = splitDisplay.join("");
-    console.log(deletedDisplay);
-    numDisplay.textContent = deletedDisplay;
+    if(equalsPressed===false){   
+        var splitDisplay = numDisplay.textContent.split("");
+        if(splitDisplay[splitDisplay.length-1]==="."){
+            decimalPressed=false;
+        }
+        if(splitDisplay[splitDisplay.length-1]==="+"
+            ||splitDisplay[splitDisplay.length-1]==="-"
+            ||splitDisplay[splitDisplay.length-1]==="x"
+            ||splitDisplay[splitDisplay.length-1]==="÷"){
+                decimalPressed=false;
+            }
+        console.log(splitDisplay);
+        splitDisplay.pop();
+        var deletedDisplay = splitDisplay.join("");
+        console.log(deletedDisplay);
+        numDisplay.textContent = deletedDisplay;
 
-    var splitSum = mySum.split("");
-    console.log(splitSum);
-    splitSum.pop();
-    var deletedSum = splitSum.join("");
-    console.log(deletedSum);
-    mySum = deletedSum;
-    //still need to fix this if used immediately after 'C' reset
+        var splitSum = mySum.split("");
+        console.log(splitSum);
+        splitSum.pop();
+        var deletedSum = splitSum.join("");
+        console.log(deletedSum);
+        mySum = deletedSum;
+        //still need to fix this if used immediately after 'C' reset
+    }
 });
-
-
